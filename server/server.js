41d1ac47258f45d0fbb0449cb1b7c4efb5c3bb0d
camillegonzales <gonzcami@oregonstate.edu>
2024-02-18 19:13:26 -0800

    moved db connector to server file

--- server/server.js
+++ server/server.js
@@ -1,14 +1,27 @@
 // Set up
 const express = require("express");
-require("./config/dbConnect");
 const usersRoute = require("./routes/usersRoute");
 const coffeeshopsRoute = require("./routes/coffeeshopsRoute");
 const reviewsRoute = require("./routes/reviewsRoute");
 const bookmarksRoute = require("./routes/bookmarksRoute");
 const dotenv = require("dotenv").config();
 const PORT = process.env.PORT;
+const mongoose = require("mongoose");
+const MONGO_URL = process.env.MONGO_URL;
 
 const app = express();
+
+// Connect to database
+const dbConnect = async () => {
+    try {
+        await mongoose.connect(MONGO_URL);
+        console.log("Successfully connected to database")
+    } catch (error) {
+        console.log("Failed to connect to database", error); 
+        process.exit(1);
+    }
+};
+dbConnect();
 
 // Middlewares
 
