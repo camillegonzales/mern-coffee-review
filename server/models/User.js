fc01760d5beea7cb06c25656a65077cd7808dbaa
camillegonzales <gonzcami@oregonstate.edu>
2024-02-18 19:41:18 -0800

    created User model

---
+++ server/models/User.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+
+// User schema
+const userSchema = new mongoose.Schema({
+    userName: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    bookmarks: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Coffeeshop' }],
+    reviews: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Review' }]
+}, {
+    timestamps: true,
+    toJSON: { virtuals: true }
+});
+
+// User model
+const User = mongoose.model("User", userSchema)
+
+module.exports = User;
