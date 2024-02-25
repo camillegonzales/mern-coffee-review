7d887cabf4ed8ff7beee1891cf4c20790d72e92e
camillegonzales <gonzcami@oregonstate.edu>
2024-02-25 15:18:15 -0800

    updated add bookmark to user

--- server/controllers/bookmarksCtrl.js
+++ server/controllers/bookmarksCtrl.js
@@ -1,3 +1,6 @@
+const CoffeeShop = require("../models/CoffeeShop");
+const User = require("../models/User");
+
 // Get all bookmarks for user
 const getBookmarksCtrl = async (req,res) => {
     try {
@@ -10,7 +13,40 @@ const getBookmarksCtrl = async (req,res) => {
 // Add bookmark to user
 const addBookmarkCtrl = async (req,res) => {
     try {
-        res.json({msg: "Add coffee shop to user's bookmarks route"});
+        const {
+            user,
+            coffeeShop
+        } = req.body
+        // 1. Find the logged in user
+        const userFound = await User.findById(req.user);
+        if (!userFound) {
+            return res.json({
+                error: "Please login to proceed"
+            });
+        }
+
+        // 2. Find the coffee shop
+        const coffeeShopFound = await CoffeeShop.findById(coffeeShop);
+        if (!coffeeShopFound) {
+            return res.json({
+                error: "Coffee shop not found"
+            });
+        }
+
+        // 3. Check if the coffee shop is already bookmarked by the user
+        if (userFound.bookmarks.includes(coffeeShopFound._id)) {
+            return res.json({ 
+                error: "Coffee shop already bookmarked" });
+        }
+
+        // 4. Add the coffee shop to the user's bookmarks
+        userFound.bookmarks.push(coffeeShopFound._id);
+        await userFound.save();
+
+        res.json({ 
+            status: "success",
+            message: "Coffee shop added to bookmarks successfully" 
+        });
     } catch (error) {
         res.json(error);
     }
