8c3eb51a5dd1fb638cc002ab23e574cd4644d9ce
camillegonzales <gonzcami@oregonstate.edu>
2024-03-16 20:18:04 -0700

    applied hamburger nav for smaller screens

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,13 +1,19 @@
 import { Link } from 'react-router-dom';
 import { authContext } from '../context/AuthContext/AuthContext';
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 
 export default function Navbar() {
     const { logoutUserAction, token } = useContext(authContext);
+    const [showMenu, setShowMenu] = useState(false);
+
+    const toggleMenu = () => {
+        setShowMenu(!showMenu);
+    };
+
     return (
-        <nav className='navbar'>
-            <h1 className='logo'>Spill The Beans</h1>
-            <div className='links'>
+        <nav className="navbar">
+            <h1 className="logo">Spill The Beans</h1>
+            <div className={`menu ${showMenu ? 'show' : ''}`}>
                 <Link to='/'>Home</Link>
                 <Link to='/search'>Search</Link>
                 {!token && <>
@@ -18,6 +24,11 @@ export default function Navbar() {
                     <Link to='/profile'>Profile</Link>
                     <button onClick={logoutUserAction}>Log out</button>
                 </>}
+            </div>
+            <div className="hamburger" onClick={toggleMenu}>
+                <div className={`bar ${showMenu ? 'open' : ''}`} />
+                <div className={`bar ${showMenu ? 'open' : ''}`} />
+                <div className={`bar ${showMenu ? 'open' : ''}`} />
             </div>
         </nav>
     );
