2435e8043f88d04579db9d0ae261eb1cff7df09e
camillegonzales <gonzcami@oregonstate.edu>
2024-03-17 16:01:20 -0700

    home page styling

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,9 +1,24 @@
 import React from "react";
+import mascotImage from "../images/STB-mascot.png";
+import sunImage from "../images/STB-sun.png";
 
 const HomePage = () => {
     return (
         <>
-            <h1>Welcome to my coffee shop review app</h1>
+        <div className="home-container">
+            <div className="home">
+                <h1>SPILL THE BEANS</h1>
+                <p>more than a rating</p>
+                <img src={mascotImage} alt="Spill The Beans main character"></img>
+            </div>
+        </div>
+        <div className="home-content">
+            <h1>Spill The Beans is a comprehensive rating platform designed to help the coffee community find what matters most to them. </h1>
+            <img src={sunImage} alt="Spill The Beans sun character" width="100" height="80"></img>
+        </div>
+        <div className="about-ratings">
+            <p>HELP ME</p>
+        </div>
         </>
     );
 };
