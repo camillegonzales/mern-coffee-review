63e9fcd0dcdc315b5bfaf0fa0e2d236ce99f2acc
camillegonzales <gonzcami@oregonstate.edu>
2024-03-17 19:51:08 -0700

    shows review count and has ratings next to img

--- client/src/pages/ShopProfile.js
+++ client/src/pages/ShopProfile.js
@@ -70,22 +70,28 @@ const ShopProfile = ({ match }) => {
     <div className='page'>
       <h1>{shop.name}</h1>
       {/* <button onClick={handleBookmarkClick}>Bookmark</button> */}
-      <img src={shop.image} alt={shop.name} />
+
+      <div className='shop-container'>
+        <div className='img-container'>
+          <img src={shop.image} alt={shop.name} />
+        </div>
+        
+        <div className='shop-ratings'>
+          <h2>Ratings:</h2>
+          <p>Coffee: {shop.coffeeRating || "N/A"}</p>
+          <p>Food: {shop.foodRating || "N/A"}</p>
+          <p>Seating: {shop.seatingRating || "N/A"}</p>
+          <p>Charging: {shop.chargingRating || "N/A"}</p>
+          <p>Noise: {shop.noiseRating || "N/A"}</p>
+        </div>
+      </div>
+
       <div className='shop-info'>
         <p>Neighborhood: {shop.neighborhood.name}</p>
         <p>Address: {shop.address}</p>
       </div>
-      
-      <div className='shop-ratings'>
-        <p>Coffee Rating: {shop.coffeeRating || "N/A"}</p>
-        <p>Food Rating: {shop.foodRating || "N/A"}</p>
-        <p>Seating Rating: {shop.seatingRating || "N/A"}</p>
-        <p>Charging Rating: {shop.chargingRating || "N/A"}</p>
-        <p>Noise Rating: {shop.noiseRating || "N/A"}</p>
-      </div>
-      
 
-      <h2>Reviews:</h2>
+      <h2>Reviews ({shopReviews ? shopReviews.length : 0}):</h2>
       <button className='search-button' onClick={handleAddReviewClick}>Add Review</button>
       <ShopReviews reviews={shopReviews} onDeleteReview={handleDeleteReview} shopID={shop.id} />
     </div>
