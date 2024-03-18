9f100faf27e56bdbbb6c7bb2d07e080d1147d9aa
camillegonzales <gonzcami@oregonstate.edu>
2024-03-17 22:37:27 -0700

    replace localhost w url

--- client/src/utils/URL.js
+++ client/src/utils/URL.js
@@ -1,7 +1,12 @@
-export const URL_USER = "http://localhost:9000/users";
+// export const URL_USER = "http://localhost:9000/users";
+export const URL_USER = "http://mern-coffee-review-backend.vercel.app/users";
 
-export const URL_REVIEWS = "http://localhost:9000/reviews";
+// export const URL_REVIEWS = "http://localhost:9000/reviews";
+export const URL_REVIEWS = "http://mern-coffee-review-backend.vercel.app/reviews";
 
-export const URL_SHOPS = "http://localhost:9000/coffeeshops";
 
-export const URL_NEIGHBORHOODS = "http://localhost:9000/neighborhoods";
+// export const URL_SHOPS = "http://localhost:9000/coffeeshops";
+export const URL_SHOPS = "http://mern-coffee-review-backend.vercel.app/coffeeshops";
+
+// export const URL_NEIGHBORHOODS = "http://localhost:9000/neighborhoods";
+export const URL_NEIGHBORHOODS = "http://mern-coffee-review-backend.vercel.app/neighborhoods";
