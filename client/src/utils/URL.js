159158b8540de33a1f46e01138b193d3c158539d
camillegonzales <gonzcami@oregonstate.edu>
2024-03-17 23:02:56 -0700

    updated URLs

--- client/src/utils/URL.js
+++ client/src/utils/URL.js
@@ -1,12 +1,12 @@
 // export const URL_USER = "http://localhost:9000/users";
-export const URL_USER = "http://mern-coffee-review-backend.vercel.app/users";
+export const URL_USER = "https://mern-coffee-review-backend.vercel.app/users";
 
 // export const URL_REVIEWS = "http://localhost:9000/reviews";
-export const URL_REVIEWS = "http://mern-coffee-review-backend.vercel.app/reviews";
+export const URL_REVIEWS = "https://mern-coffee-review-backend.vercel.app/reviews";
 
 
 // export const URL_SHOPS = "http://localhost:9000/coffeeshops";
-export const URL_SHOPS = "http://mern-coffee-review-backend.vercel.app/coffeeshops";
+export const URL_SHOPS = "https://mern-coffee-review-backend.vercel.app/coffeeshops";
 
 // export const URL_NEIGHBORHOODS = "http://localhost:9000/neighborhoods";
-export const URL_NEIGHBORHOODS = "http://mern-coffee-review-backend.vercel.app/neighborhoods";
+export const URL_NEIGHBORHOODS = "https://mern-coffee-review-backend.vercel.app/neighborhoods";
